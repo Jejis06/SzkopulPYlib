34887f326b93d9e03a2a79b7f5f571f2f0ca9855
Ignacy Buczek <wertex3233@gmail.com>
2025-01-20 16:27:23 +0100

    Update tests_szkopul.py

--- tests/tests_szkopul.py
+++ tests/tests_szkopul.py
@@ -3,15 +3,11 @@ from szkopulrequester import SkClient
 def main():
     a = SkClient(login="login", password="password",url="https://szkopul.edu.pl/c/cslom/p/")
     a.Login()
-    print(a.GetTasks(only_not_completed=True)) #pokaz zadania do zrobienia
-    print(a.SendFile("link do submita mozna zdobyc z GetTasks","trz.cpp",'C++')) wyslij plik o nazwie trz.cpp
+    print(a.GetTasks(only_not_completed=True)) # pokaz zadania do zrobienia
+    print(a.SendFile("link do submita mozna zdobyc z GetTasks","trz.cpp",'C++')) # wyslij plik o nazwie trz.cpp
     print(a.CheckLatest(all=False))
-    print(a.Ranking())#pokaz rankind
-    print(a.CheckErrors("jakis link do szczegolow"))#link do tego mozna zdobyc z check latest
-
-
-
-
+    print(a.Ranking()) # pokaz rankind
+    print(a.CheckErrors("jakis link do szczegolow")) # link do tego mozna zdobyc z check latest
 
 
 if __name__ == '__main__':
