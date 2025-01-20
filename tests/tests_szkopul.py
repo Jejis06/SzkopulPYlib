931339a62f2eae598c591ea803184a82ac826375
Ignacy Buczek <wertex3233@gmail.com>
2025-01-20 23:10:02 +0100

    Dlugo wymagany update teraz kazda funkcja dziala

--- tests/tests_szkopul.py
+++ tests/tests_szkopul.py
@@ -1,13 +1,14 @@
-from szkopulrequester import SkClient
+from szkopulpylib.szkopul import SkClient
 
 def main():
-    a = SkClient(login="login", password="password",url="https://szkopul.edu.pl/c/cslom/p/")
+    a = SkClient(login="login", password="pass",url="https://szkopul.edu.pl/c/plockie-treningi-olimpijskie/p/")
     a.Login()
-    print(a.GetTasks(only_not_completed=True)) # pokaz zadania do zrobienia
-    print(a.SendFile("link do submita mozna zdobyc z GetTasks","trz.cpp",'C++')) # wyslij plik o nazwie trz.cpp
+
+    print(a.Ranking()) # pokaz ranking
+    print(a.GetTasks()) # pokaz zadania do zrobienia
+    print(a.SendFile("link do submita danego zadania","trz.cpp",'C++')) # wyslij plik o nazwie trz.cpp
     print(a.CheckLatest(all=False))
-    print(a.Ranking()) # pokaz rankind
-    print(a.CheckErrors("jakis link do szczegolow")) # link do tego mozna zdobyc z check latest
+    print(a.CheckErrors("link")) # link do tego mozna zdobyc z check latest
 
 
 if __name__ == '__main__':
